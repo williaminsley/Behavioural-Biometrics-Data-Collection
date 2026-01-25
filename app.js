4fff84f255be8d240b7c9b453c97c49478d6e920
William Insley <williaminsley@github.com>
2026-01-25 11:15:44 +0000

    fixed app.js imports

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ import {
   summaryToCSVRow,
   downloadCSV,
   flattenFeaturesForAuth,
-  authFeaturesToCSVRow
+  authFeaturesToCSVRow,
+  generateWindows
 } from "./analysis.js";
 // ==========================
 // Utilities
